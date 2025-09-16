a81c57ac5786229aa5508f9357af0778d149a12b
Theodor Blom <me@theodorblom.com>
2025-09-16 08:25:20 +0200

    Moved configs to individual directories
    All configs now have their own directory.
    Also added config for mako.

--- fetch.sh
+++ fetch.sh
@@ -3,13 +3,15 @@
 # store explicitly installed packages
 pacman -Qe > installed.txt
 
-cp -a $XDG_CONFIG_HOME/alacritty/alacritty.toml ./configs
-cp -a $XDG_CONFIG_DIRS/fuzzel/fuzzel.ini ./configs
-cp -a /etc/greetd/config.toml ./configs
-cp -a /etc/niri/config.kdl ./configs
-cp -a $XDG_CONFIG_HOME/swaylock/config ./configs
-cp -a $HOME/images/wallpapers .
-cp -a $XDG_CONFIG_HOME/vim/vimrc ./configs
+cp -a $XDG_CONFIG_HOME/alacritty/alacritty.toml ./configs/alacritty
+cp -a $XDG_CONFIG_DIRS/fuzzel/fuzzel.ini ./configs/fuzzel
+cp -a /etc/greetd/config.toml ./configs/greetd
+cp -a /etc/niri/config.kdl ./configs/niri
+cp -a $XDG_CONFIG_HOME/swaylock/config ./configs/swaylock
+cp -a $XDG_CONFIG_HOME/vim/vimrc ./configs/vim
 cp -a $XDG_CONFIG_DIRS/waybar ./configs
-cp -a $XDG_CONFIG_HOME/bash/bashrc ./configs
-cp -a $XDG_CONFIG_DIRS/user-dirs.defaults ./configs
+cp -a $XDG_CONFIG_HOME/bash/bashrc ./configs/bash
+cp -a $XDG_CONFIG_HOME/mako/config ./configs/mako
+cp -a $XDG_CONFIG_DIRS/user-dirs.defaults ./configs/xdg-user-dirs
+
+cp -a $HOME/images/wallpapers .
