ccfbe6a850999f4b7e38abec0eab5fbaf9f7be68
Theodor Blom <me@theodorblom.com>
2025-10-02 08:26:26 +0200

    Updated vimrc
    Updated vimrc to be more complete.
    Also moved the config to be global to make room for nvim.

--- fetch.sh
+++ fetch.sh
@@ -8,7 +8,7 @@ cp -a $XDG_CONFIG_DIRS/fuzzel/fuzzel.ini ./configs/fuzzel
 cp -a /etc/greetd/config.toml ./configs/greetd
 cp -a /etc/niri/config.kdl ./configs/niri
 cp -a $XDG_CONFIG_HOME/swaylock/config ./configs/swaylock
-cp -a $XDG_CONFIG_HOME/vim/vimrc ./configs/vim
+cp -a /etc/vimrc ./configs/vim
 cp -a $XDG_CONFIG_DIRS/waybar ./configs
 cp -a $XDG_CONFIG_HOME/bash/bashrc ./configs/bash
 cp -a $XDG_CONFIG_HOME/mako/config ./configs/mako
