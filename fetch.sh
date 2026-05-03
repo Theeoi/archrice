9011c7a8ecff02985f9dcbb35fbd78f71468b01d
Theodor Blom <me@theodorblom.com>
2026-05-03 11:23:27 +0200

    Updated fetch to use rsync instead of cp
    - Fetch uses rsync to properly mirror the content
    - Deleted files are now also deleted when fetching

--- fetch.sh
+++ fetch.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
 # store explicitly installed packages
-pacman -Qe > installed.txt
+pacman -Qe >installed.txt
 
-cp -a $XDG_CONFIG_HOME/alacritty/alacritty.toml ./configs/alacritty
-cp -a $XDG_CONFIG_DIRS/fuzzel/fuzzel.ini ./configs/fuzzel
-cp -a /etc/greetd/config.toml ./configs/greetd
-cp -a /etc/niri/config.kdl ./configs/niri
-cp -a $XDG_CONFIG_HOME/swaylock/config ./configs/swaylock
-cp -a /etc/vimrc ./configs/vim
-cp -a $XDG_CONFIG_DIRS/waybar/style.css ./configs/waybar
-cp -a $XDG_CONFIG_DIRS/waybar/config.jsonc ./configs/waybar
-cp -a $XDG_CONFIG_HOME/bash/bashrc ./configs/bash
-cp -a $XDG_CONFIG_HOME/mako/config ./configs/mako
-cp -a $XDG_CONFIG_DIRS/user-dirs.defaults ./configs/xdg-user-dirs
-cp -a $XDG_CONFIG_HOME/fastfetch ./configs
-cp -a $XDG_CONFIG_HOME/nvim ./configs
-cp -a $XDG_CONFIG_HOME/latexmk ./configs
-cp -a $XDG_CONFIG_HOME/tmux/tmux.conf ./configs/tmux
-cp -a $HOME/documents/obsidian-vault/.obsidian.vimrc ./configs/obsidian/obsidian.vimrc
-cp -a $HOME/documents/obsidian-vault/.obsidian/{community-plugins,hotkeys}.json ./configs/obsidian
+rsync -a --delete $XDG_CONFIG_HOME/alacritty/alacritty.toml ./configs/alacritty
+rsync -a --delete $XDG_CONFIG_DIRS/fuzzel/fuzzel.ini ./configs/fuzzel
+rsync -a --delete /etc/greetd/config.toml ./configs/greetd
+rsync -a --delete /etc/niri/config.kdl ./configs/niri
+rsync -a --delete $XDG_CONFIG_HOME/swaylock/config ./configs/swaylock
+rsync -a --delete /etc/vimrc ./configs/vim
+rsync -a --delete $XDG_CONFIG_DIRS/waybar/style.css ./configs/waybar
+rsync -a --delete $XDG_CONFIG_DIRS/waybar/config.jsonc ./configs/waybar
+rsync -a --delete $XDG_CONFIG_HOME/bash/bashrc ./configs/bash
+rsync -a --delete $XDG_CONFIG_HOME/mako/config ./configs/mako
+rsync -a --delete $XDG_CONFIG_DIRS/user-dirs.defaults ./configs/xdg-user-dirs
+rsync -a --delete $XDG_CONFIG_HOME/fastfetch ./configs
+rsync -a --delete $XDG_CONFIG_HOME/nvim ./configs
+rsync -a --delete $XDG_CONFIG_HOME/latexmk ./configs
+rsync -a --delete $XDG_CONFIG_HOME/tmux/tmux.conf ./configs/tmux
+rsync -a --delete $HOME/documents/obsidian-vault/.obsidian.vimrc ./configs/obsidian/obsidian.vimrc
+rsync -a --delete $HOME/documents/obsidian-vault/.obsidian/{community-plugins,hotkeys}.json ./configs/obsidian
 
-cp -a /usr/local/bin/* ./scripts
+rsync -a --delete /usr/local/bin/* ./scripts
 
-cp -a $HOME/images/wallpapers .
+rsync -a --delete $HOME/images/wallpapers .
