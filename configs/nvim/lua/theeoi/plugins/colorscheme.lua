3f208ae2550bc2fb15cd2b190a92268e045585cc
Theodor Blom <me@theodorblom.com>
2026-05-03 10:52:17 +0200

    Removed unnessesary comments from nvim configs

--- configs/nvim/lua/theeoi/plugins/colorscheme.lua
+++ configs/nvim/lua/theeoi/plugins/colorscheme.lua
@@ -4,7 +4,6 @@ return {
 		lazy = false,
 		priority = 1000,
 		config = function()
-			---@type dracula
 			local dracula = require("dracula")
 
 			dracula.setup({
@@ -21,16 +20,13 @@ return {
 				},
 				transparent = true,
 				on_colors = function(colors, color)
-					---@type dracula.palette
 					return {
 						-- override or create new colors
 						-- mycolor = "#ffffff",
 					}
 				end,
 				on_highlights = function(colors, color)
-					---@type dracula.highlights
 					return {
-						---@type vim.api.keyset.highlight
 						-- Normal = { fg = colors.mycolor }
 					}
 				end,
--- configs/nvim/lua/theeoi/plugins/formatting.lua
+++ configs/nvim/lua/theeoi/plugins/formatting.lua
@@ -17,11 +17,7 @@ return {
 				desc = "Format buffer",
 			},
 		},
-		-- This will provide type hinting with LuaLS
-		---@module "conform"
-		---@type conform.setupOpts
 		opts = {
-			-- Define your formatters.
 			formatters_by_ft = {
 				lua = { "stylua" },
 				python = { "ruff_organize_imports", "ruff_format" },
@@ -31,11 +27,9 @@ return {
 				tex = { "tex-fmt" },
 				latex = { "tex-fmt" },
 			},
-			-- Set default options
 			default_format_opts = {
 				lsp_format = "never",
 			},
-			-- Set up format-on-save
 			format_on_save = {
 				async = false,
 				timeout_ms = 500,
@@ -43,7 +37,6 @@ return {
 			},
 		},
 		init = function()
-			-- If you want the formatexpr, here is the place to set it
 			vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
 		end,
 	},
